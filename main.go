91b0ebfdc2e4364c56b1f34a4149db8b0c86f90d
t.mukhortov <t.mukhortov@gmail.com>
2020-02-04 18:21:26 +0300

    First launch simple web server by tutorial

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Printf("hello, world\n")
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil)
+}
+
+// HelloServer print string started from Hello, *and path*
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
